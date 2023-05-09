2924a7eda15e453073a38b88b4c5ff3bd697f2d7
Stijn Lievens <stijn.lievens@hogent.be>
2023-05-09 15:18:40 +0200

    Make linter happy on RuleFindingConfig

--- src/shared/Configs/RuleFindingConfig.py
+++ src/shared/Configs/RuleFindingConfig.py
@@ -1,9 +1,17 @@
 import json
 from typing import Dict
-from src.shared.Enums.BinningEnum import BinningEnum 
+from src.shared.Enums.BinningEnum import BinningEnum
+
 
 class RuleFindingConfig:
-    def __init__(self, rule_length: int, min_support: float, lift: int, confidence: float, filtering_string: str, binning_option: Dict[str, BinningEnum], dropping_options : Dict[str,Dict[str, str]]):
+
+    def __init__(self,
+                 rule_length: int,
+                 min_support: float,
+                 lift: int, confidence: float,
+                 filtering_string: str,
+                 binning_option: Dict[str, BinningEnum],
+                 dropping_options: Dict[str, Dict[str, str]]):
         self.rule_length = rule_length
         self.min_support = min_support
         self.lift = lift
@@ -16,7 +24,13 @@ class RuleFindingConfig:
         return json.dumps(self.__dict__)
 
     @staticmethod
-    # def create_from_json(json_string) -> RuleFindingConfig:
     def create_from_json(json_string):
         data = json.loads(json_string)
-        return RuleFindingConfig(rule_length=data["rule_length"], min_support=data["min_support"], lift=data["lift"], confidence=data["confidence"], filtering_string=data["filtering_string"], binning_option=data["binning_option"], dropping_options=data["dropping_options"])
+        return RuleFindingConfig(
+            rule_length=data["rule_length"],
+            min_support=data["min_support"],
+            lift=data["lift"],
+            confidence=data["confidence"],
+            filtering_string=data["filtering_string"],
+            binning_option=data["binning_option"],
+            dropping_options=data["dropping_options"])
