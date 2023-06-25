842122285cecd724267749088be1a354efc02b23
Stijn Lievens <stijn.lievens@hogent.be>
2023-06-25 14:36:26 +0200

    Add some tests for fuzzy matching

--- test/backend/test_domain_controller.py
+++ test/backend/test_domain_controller.py
@@ -47,5 +47,96 @@ def test_get_all_column_rules_from_df_and_config_1():
     rule_info = json.loads(result_dict["a => b"])
     assert type(rule_info) == dict
 
+
+def test_fuzzy_match_dataprep_1():
+    """
+    Test that fuzzy_match_dataprep returns a valid response.
+    In this case, no clusters should be found.
+    """
+    df = pd.DataFrame({
+        'A': ['abc', 'aec', 'abd', 'def', 'def']
+    })
+
+    domain_controller = dc.DomainController()
+
+    result = domain_controller.fuzzy_match_dataprep(
+        dataframe_in_json=df.to_json(),
+        col='A',
+        cluster_method='levenshtein',
+        df_name="test_df",
+        ngram=None,
+        radius=1,
+        block_size=3)
+
+    assert result is not None
+    result = json.loads(result)
+    assert len(result) == 0
+
+
+def test_fuzzy_match_dataprep_2():
+    """
+    Test that fuzzy_match_dataprep returns a valid response.
+    In this case, a single cluster should be found,
+    namely abc and abc.
+    """
+    df = pd.DataFrame({
+        'A': ['abc', 'aec', 'abd', 'def', 'def']
+    })
+
+    domain_controller = dc.DomainController()
+
+    result = domain_controller.fuzzy_match_dataprep(
+        dataframe_in_json=df.to_json(),
+        col='A',
+        cluster_method='levenshtein',
+        df_name="test_df",
+        ngram=None,
+        radius=1,
+        block_size=2)
+
+    assert result is not None
+    result = json.loads(result)
+    assert len(result) == 1
+
+    for cluster_id, values in result.items():
+        assert type(cluster_id) == str
+        assert type(values) == list
+        assert len(values) == 2
+        assert sorted(values) == ['abc', 'abd']
+
+
+def test_fuzzy_match_dataprep_3():
+    """
+    Test that fuzzy_match_dataprep returns a valid response.
+    In this case, three clusters should be found,
+    namely [abc, abd], [abc, aec], [abc, abd]
+    """
+    df = pd.DataFrame({
+        'A': ['abc', 'aec', 'abd', 'def', 'def']
+    })
+
+    domain_controller = dc.DomainController()
+
+    result = domain_controller.fuzzy_match_dataprep(
+        dataframe_in_json=df.to_json(),
+        col='A',
+        cluster_method='levenshtein',
+        df_name="test_df",
+        ngram=None,
+        radius=1,
+        block_size=1)
+
+    assert result is not None
+    result = json.loads(result)
+    assert len(result) == 3
+
+    all_values = result.values()
+    sorted_values = [sorted(_) for _ in all_values]
+    # Check the clusters
+    assert ["abc", "abd", "aec"] in sorted_values
+    assert ["abc", "aec"] in sorted_values
+    assert ["abc", "abd"] in sorted_values
+
+
 if __name__ == "__main__":
     test_get_all_column_rules_from_df_and_config_1()
