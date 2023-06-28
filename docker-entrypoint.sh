c9925a47c7358fdeac3d0818b547e9c85bab1d23
MarioVerstraeten <MarioVerstraeten@outlook.com>
2023-06-28 10:39:33 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,14 @@ nginx
 
 echo "Starting gunicorn"
 gunicorn --worker-class gevent --workers=3 \
+         -t 0 \
          --access-logfile gunicorn.access.log \
          --error-logfile gunicorn.error.log \
          run_flask:app --daemon
 
 echo "Starting streamlit"
 streamlit run run_streamlit.py \
+    --browser.gatherUsageStats false \
     --server.port 8501 \
     --server.baseUrlPath /aimdmtool/ \
     --server.enableCORS true \
