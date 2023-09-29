ffb80dd80c9efd7b2c77ef10a6a8c0708121446e
Stijn Lievens <stijn.lievens@hogent.be>
2023-09-29 10:07:41 +0200

    Remove unused code from FuzzyMatcher

--- src/backend/DataCleaning/FuzzyMatcher.py
+++ src/backend/DataCleaning/FuzzyMatcher.py
@@ -4,35 +4,16 @@ Common functions and classes for the clean_duplication function.
 # pylint: disable=no-name-in-module
 from string import punctuation
 from unicodedata import combining, category, normalize
-from base64 import b64encode
 from collections import defaultdict
-from operator import itemgetter
 from typing import List, Set, Union, DefaultDict
 from itertools import permutations
-from os import path
-from tempfile import mkdtemp
 from math import ceil
 import pandas as pd
 import dask.dataframe as dd
 import dask
-from IPython.display import Javascript, display
 from metaphone import doublemetaphone
 from rapidfuzz.distance.Levenshtein import distance as LevenshteinDistance
 import config as cfg
-
-
-DECODE_FUNC = """
-    function b64DecodeUnicode(str) {
-        // Going backwards: from bytestream, to percent-encoding, to original string.
-        return decodeURIComponent(atob(str).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-    }
-"""
-
-DEFAULT_NGRAM = "2"
-DEFAULT_RADIUS = "2"
-DEFAULT_BLOCK_SIZE = "6"
 
 
 class FuzzyMatcher:
@@ -103,8 +84,11 @@ class FuzzyMatcher:
         clusters = df.groupby("key")[self._col].apply(list, meta=(self._col, "object"))
         clusters = clusters.loc[clusters.map(len) > 1]
         clusters, self._counts = dask.compute(clusters, counts)
+
+        cfg.logger.debug("FuzzyMatcher: _key_collision_cluster: clusters = %s", clusters)
         # sort by the size of each cluster, so that larger clusters appear first
-        self.clusters = clusters.sort_values(key=lambda x: x.map(len), ascending=False).reset_index(
+        self.clusters = clusters.sort_values(
+            key=lambda x: x.map(len), ascending=False).reset_index(
             drop=True
         )
 
@@ -219,121 +203,6 @@ class FuzzyMatcher:
         https://github.com/OpenRefine/OpenRefine/wiki/Clustering-In-Depth
         """
         return "".join(sorted(set(_ngram_tokens(val, self._ngram))))
-
-    def _create_replace_calls(
-        self, cluster_page: pd.Series, do_merge: List[bool], new_values: List[str]
-    ) -> str:
-
-        """
-        Creates a string containing the required replace function calls.
-
-        cluster_page
-            Current page of clusters being displayed by the UI
-        do_merge
-            Boolean values indicating whether a cluster should be merged.
-            If do_merge[i] == True then merge the i-th cluster.
-        """
-        df_name, col = self._df_name, f"'{self._col}'"
-        replace_calls = []
-        for idx, cluster in enumerate(cluster_page):
-            if do_merge[idx]:
-                # create the string that all the values in the cluster will be set to
-                cluster_repr = new_values[idx].replace("'", "\\'")
-                cluster_repr = f"'{cluster_repr}'"
-                # create the strings to be replaced
-                cluster_vals = [val.replace("'", "\\'") for val, _ in cluster]
-                cluster_vals = [f"'{val}'" for val in cluster_vals if f"'{val}'" != cluster_repr]
-                code = (
-                    f"{df_name}[{col}] = {df_name}[{col}].replace"
-                    f"([{', '.join(cluster_vals)}], {cluster_repr})"
-                )
-                replace_calls.append(code)
-
-        return "\n" + "\n".join(replace_calls)
-
-    def live_export_code(
-        self, cluster_page: pd.Series, do_merge: List[bool], new_values: List[str]
-    ) -> None:
-        """
-        Create DataFrame replace calls to merge clusters and output
-        them into the jupyter notebook.
-        """
-        code = self._create_replace_calls(cluster_page, do_merge, new_values)
-        encoded_code = (b64encode(str.encode(code))).decode()
-
-        code = f"""
-            {DECODE_FUNC}
-            var ind = IPython.notebook.get_selected_index();
-            var cell = IPython.notebook.get_cell(ind);
-            var text = cell.get_text();
-            cell.set_text(text.concat(b64DecodeUnicode("{encoded_code}")));
-        """
-        display(Javascript(code))
-
-    def execute_merge_code(
-        self,
-        cluster_page: pd.Series,
-        do_merge: List[bool],
-        new_values: List[str],
-    ) -> None:
-        """
-        Merge the clusters in the DataFrame.
-        """
-        for idx, cluster in enumerate(cluster_page):
-            cluster_repr = new_values[idx]
-            if do_merge[idx]:
-                self._df[self._col] = self._df[self._col].replace(
-                    [cluster_val for cluster_val, _ in cluster], cluster_repr
-                )
-
-    def final_df(self) -> None:
-        """
-        Displays a DataFrame with the final values in the next notebook cell.
-        Writes the final dataframe to a pickle file then reads the file from
-        inside the IPython kernel.
-        """
-        code = f"# dataframe with cleaned string values\n{self._df_name}_clean"
-        encoded_code = (b64encode(str.encode(code))).decode()
-        final_df = self._df.compute()
-        # create a temporary directory for the dataframe file
-        tmp_dir = mkdtemp().replace("\\", "/")
-        df_file = path.join(tmp_dir, "clean_duplication_output.pkl").replace("\\", "/")
-        final_df.to_pickle(df_file)
-        # code to read the file and delete the temporary directory afterwards
-        execute_code = (
-            "import pandas as pd\n"
-            "import shutil\n"
-            f"{self._df_name}_clean = pd.read_pickle('{df_file}')\n"
-            f"shutil.rmtree('{tmp_dir}')"
-        )
-        encoded_execute = (b64encode(str.encode(execute_code))).decode()
-        code = f"""
-                 {DECODE_FUNC}
-                 IPython.notebook.kernel.execute(b64DecodeUnicode("{encoded_execute}"));
-                 var code = IPython.notebook.insert_cell_below('code');
-                 code.set_text(b64DecodeUnicode("{encoded_code}"));
-                 code.execute();
-             """
-        display(Javascript(code))
-
-    def get_page(self, start: int, end: int) -> pd.Series:
-        """
-        Returns a page of clusters from start to end. Adds the counts to
-        each cluster entry and sorts each cluster by the counts, so that values
-        with a greater count appear first in the cluster.
-        """
-        page = self.clusters.iloc[start:end]
-        page = page.map(lambda cluster: [(val, self._counts.loc[val]) for val in cluster])
-        page = page.map(lambda x: sorted(x, key=itemgetter(1), reverse=True))
-        return page
-
-    def set_cluster_params(self, ngram: int, radius: int, block_size: int) -> None:
-        """
-        Set clustering parameters.
-        """
-        self._ngram = ngram
-        self._radius = radius
-        self._block_size = block_size
 
 
 def _ngram_tokens(val: str, n: int) -> List[str]:
